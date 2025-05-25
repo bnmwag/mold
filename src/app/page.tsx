3c1943d71a72582e69e7646b0e32882845bafb2c
bnmwag <bnm.wag@gmail.com>
2025-05-26 00:47:32 +0200

    feat: build out header of landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 import { Wrapper } from "@/components/layout/wrapper";
+import { Split } from "@/components/motion/split";
 import type { NextPage } from "next";
 
 const IndexPage: NextPage = () => {
 	return (
 		<Wrapper>
-			<div className="grid grid-cols-12">
-				<h1 className="col-span-8 col-start-5 indent-[25%] text-[5vw] leading-none uppercase">
-					Crafting Timeless Websites With Grace And Precision
-				</h1>
-			</div>
+			<section className="relative h-[80svh] bg-primary pt-[calc(var(--gap)*2)]">
+				<div className="dr-layout-grid h-full py-gap">
+					<Split type="lines" delay={0.2}>
+						<h1 className="col-span-8 col-start-5 indent-[25%] uppercase">Elevating Digital Experiences With Precision & Soul</h1>
+					</Split>
+					<Split type="lines" delay={0.8}>
+						<p className="col-span-4 col-start-1 text-balance uppercase content-end-safe">
+							Build using Next.js, Payload CMS, Tailwind CSS, GSAP, and TypeScript.
+						</p>
+					</Split>
+				</div>
+			</section>
+			{/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+			<section className="h-svh bg-white"></section>
 		</Wrapper>
 	);
 };
---
+++ src/components/motion/split.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { cloneElement, useRef, type FC, Children } from "react";
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+interface ISplitProps extends React.HTMLAttributes<HTMLDivElement> {
+	animationOnScroll?: boolean;
+	delay?: number;
+	start?: string;
+	markers?: boolean;
+	type?: "lines" | "words" | "chars";
+}
+
+export const Split: FC<ISplitProps> = ({
+	children,
+	animationOnScroll = false,
+	delay = 0,
+	start = "top 75%",
+	type = "lines",
+	markers = false,
+}) => {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const elementRef = useRef([]);
+	const splitRef = useRef<SplitText[]>([]);
+	const linesRef = useRef<HTMLDivElement[]>([]);
+
+	useGSAP(
+		() => {
+			if (!containerRef.current) return;
+
+			splitRef.current = [];
+			elementRef.current = [];
+			linesRef.current = [];
+
+			let elements = [];
+			if (containerRef.current.hasAttribute("data-split-wrapper")) {
+				elements = Array.from(containerRef.current.children);
+			} else {
+				elements = [containerRef.current];
+			}
+
+			for (const element of elements) {
+				//@ts-ignore
+				elementRef.current.push(element);
+
+				const split = SplitText.create(element, {
+					type,
+					mask: "lines",
+					wordsClass: "word++",
+					charsClass: "char++",
+					linesClass: "line++",
+				});
+
+				splitRef.current.push(split);
+
+				const { textIndent } = getComputedStyle(element);
+
+				if (textIndent && textIndent !== "0px") {
+					if (split.lines.length > 0) {
+						//@ts-ignore
+						split.lines[0].style.paddingLeft = textIndent;
+					}
+					//@ts-ignore
+					element.style.textIndent = "0";
+				}
+
+				//@ts-ignore
+				linesRef.current.push(...split.lines);
+			}
+
+			gsap.set(linesRef.current, { y: "100%" });
+			containerRef.current.removeAttribute("data-hidden-on-init");
+
+			const animation = {
+				y: "0%",
+				duration: 1,
+				stagger: 0.1,
+				ease: "expo.out",
+				delay,
+			};
+
+			if (animationOnScroll) {
+				gsap.to(linesRef.current, {
+					...animation,
+					scrollTrigger: {
+						trigger: containerRef.current,
+						start,
+						markers,
+						once: true,
+					},
+				});
+			} else {
+				gsap.to(linesRef.current, animation);
+			}
+
+			return () => {
+				for (const split of splitRef.current) {
+					if (split) split.revert();
+				}
+			};
+		},
+		{
+			scope: containerRef,
+			dependencies: [animationOnScroll, delay],
+		},
+	);
+
+	if (Children.count(children) === 1) {
+		//@ts-ignore
+		return cloneElement(children, {
+			ref: containerRef,
+			"data-hidden-on-init": true,
+		});
+	}
+
+	return (
+		<div ref={containerRef} data-split-wrapper data-hidden-on-init>
+			{children}
+		</div>
+	);
+};
