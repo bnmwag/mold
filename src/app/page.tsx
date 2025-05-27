360f206f1e5a62e5c09be55d7748e66d026d1d34
bnmwag <bnm.wag@gmail.com>
2025-05-27 22:52:02 +0200

    feat: introduce features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,91 @@ import FlowFieldScene from "@/components/flow-field";
 import { Wrapper } from "@/components/layout/wrapper";
 import { ParallaxImage } from "@/components/motion/parallax-image";
 import { Split } from "@/components/motion/split";
+import { randId } from "@/lib/utils";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import type { NextPage } from "next";
 import { useRef } from "react";
 
+const features = [
+	{
+		title: "performance",
+		description:
+			"Fast by default — Mold uses the latest web tech to deliver smooth, high-performance experiences without the bloat.",
+	},
+	{
+		title: "motion",
+		description:
+			"Powered by GSAP and Lenis, every animation and scroll interaction feels fluid, expressive, and deeply intentional.",
+	},
+	{ empty: true },
+	{
+		title: "freedom",
+		description:
+			"Built on Next.js, Mold gives you the flexibility to build anything — from landing pages to full-scale apps, fast.",
+	},
+	{ empty: true },
+	{
+		title: "clarity",
+		description: "Clean, modular code means fewer decisions, better focus, and less time fighting your own setup.",
+	},
+	{
+		title: "scalability",
+		description: "Start small or go big — Mold adapts effortlessly to projects of any size, without added complexity.",
+	},
+];
+
 const IndexPage: NextPage = () => {
+	const uspContainerRef = useRef<HTMLDivElement>(null);
 	const featuresContainerRef = useRef<HTMLDivElement>(null);
 
 	useGSAP(
 		() => {
-			if (!featuresContainerRef.current) return;
+			if (!uspContainerRef.current) return;
 
-			const words = featuresContainerRef.current.querySelectorAll(".word");
+			const words = uspContainerRef.current.querySelectorAll(".word");
 
 			gsap.set(words, { x: "100vw" });
 			gsap.to(words, {
 				x: "0%",
 				duration: 1,
 				stagger: 0.1,
-				ease: "power2.out",
+				ease: "expo.out",
 				scrollTrigger: {
-					trigger: featuresContainerRef.current,
+					trigger: uspContainerRef.current,
 					start: "top 80%",
 					end: "bottom 50%",
 					scrub: true,
 				},
 			});
+		},
+		{ scope: uspContainerRef },
+	);
+
+	useGSAP(
+		() => {
+			if (!featuresContainerRef.current) return;
+
+			const features = featuresContainerRef.current.querySelectorAll(".col-span-3");
+
+			for (const f of features) {
+				const index = Number(f.getAttribute("data-index"));
+
+				const multiPattern = [0, 1, 2, 1];
+				const multi = multiPattern[index % multiPattern.length];
+
+				gsap.from(f, {
+					y: `${multi * 500}px`,
+					duration: 1,
+					ease: "power1.out",
+					scrollTrigger: {
+						trigger: featuresContainerRef.current,
+						start: "top bottom",
+						end: "bottom bottom-=50%",
+						scrub: true,
+					},
+				});
+			}
 		},
 		{ scope: featuresContainerRef },
 	);
@@ -62,7 +120,7 @@ const IndexPage: NextPage = () => {
 			</section>
 			<section className="relative h-screen">
 				<div className="relative h-full py-gap">
-					<div className="flex h-full items-center overflow-hidden" ref={featuresContainerRef}>
+					<div className="flex h-full items-center overflow-hidden" ref={uspContainerRef}>
 						<Split type="words" disableAnimation>
 							<div className="dr-layout-grid">
 								<h2 className="h2 | col-span-10 text-balance uppercase" data-text>
@@ -71,6 +129,35 @@ const IndexPage: NextPage = () => {
 								</h2>
 							</div>
 						</Split>
+					</div>
+				</div>
+			</section>
+			<section className="relative mb-[calc(var(--gap)*12)]">
+				<div className="relative h-full py-gap">
+					<div className="dr-layout-grid gap-y-[calc(var(--gap)*4)]" ref={featuresContainerRef}>
+						{features.map((feature, index) =>
+							feature.empty ? (
+								<div className="col-span-3" key={randId()} />
+							) : (
+								<div
+									key={feature.title}
+									className="col-span-3 aspect-[3/3.5] bg-[#0f0f0f] p-gap text-secondary"
+									data-index={index}
+								>
+									<div className="flex items-start justify-between">
+										<p className="">
+											FTR
+											<br /> 00{index + 1}
+										</p>
+										<div className="size-4 bg-contrast" />
+									</div>
+									<div className="flex h-full flex-col justify-between py-24">
+										<div className="h3">{feature.title}</div>
+										<p className="indent-[25%]">{feature.description}</p>
+									</div>
+								</div>
+							),
+						)}
 					</div>
 				</div>
 			</section>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
 export const lerp = (a: number, b: number, t: number) => {
 	return a + (b - a) * t;
 };
+
+export const randId = () => Math.random().toString(36).substring(2, 9);
