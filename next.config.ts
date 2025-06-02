35b4409df28ea118568ecf9229bd2ca1ce80e6ae
bnmwag <bnm.wag@gmail.com>
2025-06-02 22:21:02 +0200

    fix: return URL directly w/o remote pattern obj

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,11 @@ import type { NextConfig } from "next";
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 	? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-	: undefined || process.env.__NEXT_PRIVATE_ORIGIN || "http://localhost:3000";
+	: process.env.__NEXT_PRIVATE_ORIGIN || "http://localhost:3000";
 
 const nextConfig: NextConfig = {
 	images: {
-		remotePatterns: [
-			...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-				const url = new URL(item);
-
-				return {
-					hostname: url.hostname,
-					protocol: url.protocol.replace(":", ""),
-				};
-			}),
-		],
+		remotePatterns: [...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => new URL(item))],
 	},
 };
 
