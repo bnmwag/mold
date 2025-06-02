ef57ce5e0e5e5ba3f1d1d72fbc4d83579193b640
bnmwag <bnm.wag@gmail.com>
2025-06-02 22:07:45 +0200

    fix: delay text entry

--- src/heros/high-impact.tsx
+++ src/heros/high-impact.tsx
@@ -8,10 +8,10 @@ export const HighImpact: FC<Page["hero"]> = ({ content, title }) => {
 	return (
 		<section className="relative h-[80svh] bg-primary pt-[calc(var(--gap)*2)]">
 			<div className="dr-layout-grid h-full py-gap">
-				<Split type="lines">
+				<Split type="lines" delay={0.25}>
 					<h1 className="dr-text-78 col-span-8 col-start-5 indent-[25%] uppercase">{title}</h1>
 				</Split>
-				<Split type="lines" delay={0.25}>
+				<Split type="lines" delay={0.5}>
 					<p className="col-span-6 col-start-1 text-balance content-end-safe">{content}</p>
 				</Split>
 			</div>
