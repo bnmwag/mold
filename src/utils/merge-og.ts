fd1633b1781cd884d5bbe3637d6651ef9209d9c3
bnmwag <bnm.wag@gmail.com>
2025-06-02 21:49:14 +0200

    fix: update default payload template website to mold

--- src/utils/merge-og.ts
+++ src/utils/merge-og.ts
@@ -10,8 +10,8 @@ const defaultOpenGraph: Metadata["openGraph"] = {
 				: "/website-template-OG.webp",
 		},
 	],
-	siteName: "Payload Website Template",
-	title: "Payload Website Template",
+	siteName: "Mold",
+	title: "Mold",
 };
 
 export const mergeOpenGraph = (og?: Metadata["openGraph"]): Metadata["openGraph"] => {
