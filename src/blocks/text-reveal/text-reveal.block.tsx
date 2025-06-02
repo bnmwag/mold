d9de2ebfcc79e2432a6fd5693fb719031d39e25d
bnmwag <bnm.wag@gmail.com>
2025-06-02 22:08:35 +0200

    fix: rm custom animation

--- src/blocks/text-reveal/text-reveal.block.tsx
+++ src/blocks/text-reveal/text-reveal.block.tsx
@@ -10,38 +10,15 @@ import { useEntryAnimation } from "@/lib/transitions";
 export const TextRevealBlock: FC<ITextRevealBlockProps> = ({ content }) => {
 	const uspContainerRef = useRef<HTMLDivElement>(null);
 
-	useEntryAnimation(
-		() => {
-			if (!uspContainerRef.current) return;
-
-			const words = uspContainerRef.current.querySelectorAll(".word");
-
-			gsap.set(words, { x: "100vw" });
-			gsap.to(words, {
-				x: "0%",
-				duration: 1,
-				stagger: 0.1,
-				ease: "expo.out",
-				scrollTrigger: {
-					trigger: uspContainerRef.current,
-					start: "top 80%",
-					end: "bottom 50%",
-					scrub: true,
-				},
-			});
-		},
-		{ scope: uspContainerRef },
-	);
-
 	return (
 		<section className="relative h-screen">
 			<div className="relative h-full py-gap">
 				<div className="flex h-full items-center overflow-hidden" ref={uspContainerRef}>
-					<Split type="words" disableAnimation>
-						<div className="dr-layout-grid">
-							<h2 className="h2 | col-span-10 text-balance uppercase">{content}</h2>
-						</div>
-					</Split>
+					<div className="dr-layout-grid">
+						<Split type="lines" animationOnScroll>
+							<h2 className="h2 | col-span-10 text-balance indent-[25%] uppercase">{content}</h2>
+						</Split>
+					</div>
 				</div>
 			</div>
 		</section>
