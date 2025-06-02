4b5f577dc26a628853c2a66723ed5ccf7df4b44c
bnmwag <bnm.wag@gmail.com>
2025-06-02 22:20:24 +0200

    fix: put nullish coalescing operator to make ts happy

--- src/plugins/s3-storage-plugin.ts
+++ src/plugins/s3-storage-plugin.ts
@@ -2,13 +2,13 @@ import { s3Storage } from "@payloadcms/storage-s3";
 
 export const storage_plugin = s3Storage({
 	collections: { media: true },
-	bucket: process.env.PAYLOAD_PUBLIC_S3_BUCKET,
+	bucket: process.env.PAYLOAD_PUBLIC_S3_BUCKET!,
 	config: {
 		forcePathStyle: true,
 		endpoint: process.env.PAYLOAD_PUBLIC_S3_ENDPOINT,
 		credentials: {
-			accessKeyId: process.env.PAYLOAD_PUBLIC_S3_ACCESS_KEY,
-			secretAccessKey: process.env.PAYLOAD_PUBLIC_S3_SECRET_KEY,
+			accessKeyId: process.env.PAYLOAD_PUBLIC_S3_ACCESS_KEY!,
+			secretAccessKey: process.env.PAYLOAD_PUBLIC_S3_SECRET_KEY!,
 		},
 		region: process.env.PAYLOAD_PUBLIC_S3_REGION,
 	},
